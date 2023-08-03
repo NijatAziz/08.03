29f9dff968e5f3ea383daeda7cbbcf408cd109df
NijatAziz <nijatfaz@code.edu.az>
2023-08-03 23:06:57 +0400

    first commit

---
+++ Homework/Extension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework
+{
+    internal static class Extension
+    {
+        public static bool CustomContains(this string mainText, string searchText)
+        {
+            string mainLower = mainText.ToLower();
+            string searchLower = searchText.ToLower();
+
+            int index = 0;
+            foreach (char c in mainLower)
+            {
+                if (c == searchLower[index])
+                {
+                    index++;
+                    if (index == searchLower.Length)
+                        return true;
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+
+            return false;
+        }
+    }
+
+
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using Homework;
+
+internal class Program
+{
+    private static void Main()
+    {
+        string mainText = "Bu bir custom contains methodudur";
+
+        string searchText = "XX";
+        bool contains = mainText.CustomContains(searchText);
+        Console.WriteLine($"'{searchText}' cumlede ardicil olaraq varmi? {contains}");
+    }
+}
